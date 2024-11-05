1812a45158d3c0224696cb1af7bb6e6407780a1a
iMythms <maythams.149@gmail.com>
2024-11-05 13:10:53 +0300

    Add car management features with routes and views for adding, editing, and displaying cars

--- server.js
+++ server.js
@@ -12,9 +12,20 @@ mongoose.connection.on('connected', () => {
 	console.log(`Connected to MongoDB ${mongoose.connection.name}`)
 })
 
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 app.use(methodOverride('_method'))
+
+app.use(carRoute)
+
+app.get('/', async (req, res) => {
+	res.render('index.ejs')
+})
+
+app.get('/cars/addCar', (req, res) => {
+	res.render('cars/addCar.ejs')
+})
 
 app.listen(PORT, () => {
 	console.log(`Running on localhost:${PORT}`)
