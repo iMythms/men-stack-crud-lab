46815734ac9bb3f6212c78ffbb1ac25174e93489
iMythms <maythams.149@gmail.com>
2024-11-05 14:06:31 +0300

    Add model field to car schema and update views for car details

--- models/cars.js
+++ models/cars.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const carSchema = new mongoose.Schema({
 	type: String,
 	brand: String,
+	model: String,
 	color: String,
 	year: Number,
 })
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
+const carRoute = require('./routes/cars')
 const morgan = require('morgan')
 
 const app = express()
